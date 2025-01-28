c5475298387b10eb511bd26bdf44560fea4fb080
keyur0811 <keyurambekar@gmail.com>
2025-01-28 11:16:11 -0500

    merged frontend branch to main

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# careconnect
+# Careconnect
+
+# Login Page Credentials
+User: test@example.com
+Password: password123
+
+# Use this to run fronend side of the project on your local system
+Name: Live Server
+https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const badge = document.querySelector(".badge");
-  const unreadCount = document.querySelectorAll("#notifications-list li").length;
-  badge.textContent = unreadCount;
+// document.addEventListener("DOMContentLoaded", () => {
+//   const badge = document.querySelector(".badge");
+//   const unreadCount = document.querySelectorAll("#notifications-list li").length;
+//   badge.textContent = unreadCount;
 
-  const markAsReadButtons = document.querySelectorAll(".mark-as-read");
-  markAsReadButtons.forEach((button) => {
-      button.addEventListener("click", () => {
-          const parentLi = button.parentElement;
-          parentLi.remove();
-          const updatedUnreadCount = document.querySelectorAll("#notifications-list li").length;
-          badge.textContent = updatedUnreadCount;
-      });
-  });
-});
+//   const markAsReadButtons = document.querySelectorAll(".mark-as-read");
+//   markAsReadButtons.forEach((button) => {
+//       button.addEventListener("click", () => {
+//           const parentLi = button.parentElement;
+//           parentLi.remove();
+//           const updatedUnreadCount = document.querySelectorAll("#notifications-list li").length;
+//           badge.textContent = updatedUnreadCount;
+//       });
+//   });
+// });
