3522c7138acddc9dcefb190c32b25a6d4e159fef
vinay s <svreddy199@gmail.com>
2025-02-04 10:56:03 -0500

    added cicd

--- terraformvinay/main.tf
+++ terraformvinay/main.tf
@@ -70,3 +70,132 @@ resource "aws_iam_role" "ecs_service_role" {
     ]
   })
 }
+
+resource "aws_ecr_repository" "flask_app" {
+  name = "flask-app"
+}
+
+resource "aws_ecs_cluster" "flask_cluster" {
+  name = "flask-cluster"
+}
+
+resource "aws_ecs_task_definition" "flask_task" {
+  family                   = "flask-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "flask-container"
+      image     = "${aws_ecr_repository.flask_app.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 5000
+          hostPort      = 5000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "flask_service" {
+  name            = "flask-service"
+  cluster         = aws_ecs_cluster.flask_cluster.id
+  task_definition = aws_ecs_task_definition.flask_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = ["subnet-0bb11141ce5f8158e", "subnet-0cfe4254d62a6bca8", "subnet-0a8521dbdbb8d76dc", "subnet-080b9e9f86e2f4122", "subnet-044b7ca9192bb7302", "subnet-056badd094e34a8c4"]  # Replace with actual subnet IDs
+    security_groups  = ["sg-0c7ae7f959526a48e"]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_security_group" "flask_sg" {
+  name        = "flask-sg"
+  description = "Allow inbound traffic"
+  vpc_id      = "vpc-034f418c442e4d334"  # Replace with your VPC ID
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsTaskRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_ecs_task_definition" "flask_task_update" {
+  family                   = "flask-task-update"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "flask-container-update"
+      image     = "${aws_ecr_repository.flask_app.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 5000
+          hostPort      = 5000
+        }
+      ]
+    }
+  ])
+}
