ef7839f6554729d44b16174a2e8f764efe2365c7
vinay s <svreddy199@gmail.com>
2025-02-13 23:19:04 -0500

    added cicd

--- backend/run.py
+++ backend/run.py
@@ -57,4 +57,4 @@ def patient_profile():
 
 # ✅ Run the Flask App (No Routes After This)
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
